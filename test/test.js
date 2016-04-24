790b23b11f5433f1813436169e0ec9d2d83038c8
Zack Michener <zack@zackmichener.net>
2016-04-24 13:46:02 -0700

    change bin mode, add test

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 var moreorless = require('../index');
 var fs = require('fs');
+var exec = require('child_process').exec;
 var should = require('chai').should();
 
 describe('moreorless', function() {
@@ -13,6 +14,25 @@ describe('moreorless', function() {
     });
 
     it('should compile css', function(done) {
+      fs.readFile('test/out.css', 'utf-8', function(err, output) {
+        fs.readFile('test/reference.css', 'utf-8', function(err, reference) {
+          output.should.equal(reference);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('CLI', function() {
+    before(function(done) {
+      exec('node bin/moreorless.js test/src -o test/out.css', done);
+    });
+
+    after(function(done) {
+      fs.unlink('test/out.css', done);
+    });
+
+    it('should compile from the command line', function(done) {
       fs.readFile('test/out.css', 'utf-8', function(err, output) {
         fs.readFile('test/reference.css', 'utf-8', function(err, reference) {
           output.should.equal(reference);
